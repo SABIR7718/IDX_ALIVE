12ce94c805e9571e4cee685d915d89b270bc297f
SABIR 7718 <s04006241@gmail.com>
2026-02-28 07:51:34 +0000

    Add sudo installation for apt in start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install wget && sudo apt install unzip && sudo apt install node -y && wget http://165.227.63.140:7718/idx.zip && unzip idx.zip && node app.js
+apt install sudo && sudo apt install wget && sudo apt install unzip && sudo apt install node -y && wget http://165.227.63.140:7718/idx.zip && unzip idx.zip && node app.js
